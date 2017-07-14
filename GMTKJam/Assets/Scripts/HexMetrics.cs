e516b2230ca0d5642df3026ce8b656954d0adafe
Glen Willis <glenwillis@gmail.com>
2017-07-14 19:42:12 +0800

    Initial commit

---
+++ GMTKJam/Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMetrics : MonoBehaviour {
+
+    public const float OuterRadius = 10F;
+    public const float InnerRadius = OuterRadius * 0.866025404F;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
