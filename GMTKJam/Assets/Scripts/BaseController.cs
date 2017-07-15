7399c1507b021a02d7935c40b48d63fa8d090712
Glen Willis <glenwillis@gmail.com>
2017-07-15 20:21:37 +0800

    Base is being added to dictionary now as well.

--- GMTKJam/Assets/Scripts/BaseController.cs
+++ GMTKJam/Assets/Scripts/BaseController.cs
@@ -7,11 +7,20 @@ public class BaseController : MonoBehaviour {
     public int NumberOfActionsRemaining;
     private UnitTypes ut;
     public string Name;
+    public HexCoordinates hexCoords;
+    HexGrid hexGrid;
 
     void Start()
     {
         ut = GetComponent<ObjectInfo>().ut;
+        hexGrid = FindObjectOfType<HexGrid>();
+        SetHexCoords();
+        OverallHexCoordsDict.GameDictionary.Add(hexCoords, ut);
     }
 
+    private void SetHexCoords()
+    {
+        hexCoords = hexGrid.ReturnHexCoords(transform.position);
+    }
 
 }
